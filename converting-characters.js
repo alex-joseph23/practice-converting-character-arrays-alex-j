b38ee19e841264f0fe869600da6356e511ee7a63
Alex Joseph <alexandra.k.joseph2323@gmail.com>
2025-03-31 12:29:25 -0500

    message decoding

--- converting-characters.js
+++ converting-characters.js
@@ -1,1 +1,26 @@
-found convert repo
+let message1 = " !yako eb ll'uoy dna ,gniog peeK !sgnittes fo yteirav a ni slliks gnidoc esu osla nac uoY !ti teg ll'uoy ,tsisrep uoy fi tub ,tsrif ta drah mees yam gnidoC";
+let message2 = "!ecitcarp htiw retteb teg ll‘uoy ,emit ekaT .tsrif ta drah leef lliw gnitirw edoc tuB";
+let message3 = "!elpoep rehto morf tnereffid on era uoy ,elbuort evah uoy fI .lanoisseforp a ekil leef ot evah t'nod uoY";
+let message4 = ".rettam llits yeht ,smargorp llams etirw ylno nac uoy fI .tnemom tcefrep eht rof tiaw t'noD .yadot trats tsuJ";
+
+let decodedMessage1 = message1.split('').reverse().join('');
+let decodedMessage2 = message2.split('').reverse().join('');
+let decodedMessage3 = message3.split('').reverse().join('');
+let decodedMessage4 = message4.split('').reverse().join('');
+
+console.log(decodedMessage1);
+console.log(decodedMessage2);
+console.log(decodedMessage3);
+console.log(decodedMessage4);
+
+let myMessage1 = "When life gives you lemons, make lemonade.";
+let myMessage2 = "You never know where life will take you.";
+let myMessage3 = "YOLO: you only live once.";
+
+let reversedMyMessage1 = myMessage1.split('').reverse().join('');
+let reversedMyMessage2 = myMessage2.split('').reverse().join('');
+let reversedMyMessage3 = myMessage3.split('').reverse().join('');
+
+console.log(reversedMyMessage1);
+console.log(reversedMyMessage2);
+console.log(reversedMyMessage3);
